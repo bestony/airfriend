e1caae018482501036bc30d159cd5ba1b29a4e7b
Bestony <best.tony@foxmail.com>
2019-06-10 10:18:40 +0800

    Initial commit

---
+++ AirFriend/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AirFriend
+//
+//  Created by Bestony on 2019/6/10.
+//  Copyright © 2019 Bestony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
