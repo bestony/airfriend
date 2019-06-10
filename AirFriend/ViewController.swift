11abd08cbb81913b58876d9f760b8f493ac2561c
Bestony <best.tony@foxmail.com>
2019-06-10 12:41:05 +0800

    feat: add Alamofire sample code

--- AirFriend/ViewController.swift
+++ AirFriend/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        AF.request("https://httpbin.org/get").responseJSON { response in
+            print("Request: \(String(describing: response.request))")   // original url request
+            print("Response: \(String(describing: response.response))") // http url response
+            print("Result: \(response.result)")                         // response serialization result
+            
+            
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                print("Data: \(utf8Text)") // original server data as UTF8 string
+            }
+        }
     }
 
 
